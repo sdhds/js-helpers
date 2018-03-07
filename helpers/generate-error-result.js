2df2fae01c2d7249049580d923cc37af4f562ce3
Anders0l <i@andersol.cc>
2018-03-07 14:51:32 +0300

    add error logging

--- helpers/generate-error-result.js
+++ helpers/generate-error-result.js
@@ -1,6 +1,13 @@
 const httpCodeErrors = require('./http-code-errors');
 
-module.exports = (res, type) => {
+module.exports = (res, type, stackTrace) => {
 	const error = httpCodeErrors(type);
+	if (error.code != 404) {
+		const logingError = error;
+		if (stackTrace) {
+			logingError.stackTrace = (typeof stackTrace === 'object') ? JSON.stringify(stackTrace) : stackTrace;
+		}
+		console.error(logingError);
+	}
 	return res.status(error.code).json(error);
 }
