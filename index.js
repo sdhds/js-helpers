684cf030f0763de60a780fe82d94adbd9ebc9466
Anders0l <i@andersol.cc>
2018-03-09 18:18:54 +0300

    add generateReqId for frontend

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const generateUniqId = require('./helpers/generate-uniq-id');
+const generateReqId = require('./helpers/generate-req-id');
 const httpCodeErrors = require('./helpers/http-code-errors');
 const generateErrorResult = require('./helpers/generate-error-result');
 
 module.exports = {
+	generateReqId, //use it to generate request id at frontend
 	generateUniqId,
 	httpCodeErrors,
 	generateErrorResult
