49c205f8ae6ded3345699d052e654451e46f37d0
Anders0l <i@andersol.cc>
2018-03-09 19:26:43 +0300

    add uppercase

--- helpers/generate-req-id.js
+++ helpers/generate-req-id.js
@@ -6,6 +6,6 @@ module.exports = () => {
 	return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
 		const r = (d + Math.random() * 16) % 16 | 0;
 		d = Math.floor(d / 16);
-		return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+		return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16).toUpperCase()
 	});
 }
