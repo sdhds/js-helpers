d677cfd7138601cf50f405e34ffcd3ff3e32cc3c
Tais1990 <tais_kat@list.ru>
2018-04-25 14:56:08 +0500

    Bug fix

--- helpers/find-first-video.js
+++ helpers/find-first-video.js
@@ -1,12 +1,12 @@
-const YTVideoIdRegexp = /(youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]+)/;
-const VMVideIdRegexp = /(?:www\.|player\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/(?:[^/]*)\/videos\/|album\/(?:\d+)\/video\/|video\/|)(\d+)(?:[a-zA-Z0-9_-]+)?/i;
-	
+const YTVideoIdRegexp = /(http[s]?:\/\/){0,1}(?:www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]+)/;
+const VMVideIdRegexp = /(http[s]?:\/\/){0,1}(?:www\.|player\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/(?:[^/]*)\/videos\/|album\/(?:\d+)\/video\/|video\/|)(\d+)(?:[a-zA-Z0-9_-]+)?/i;
+			
 module.exports = (str) => {
 	const resultYT = str.match( YTVideoIdRegexp );
 	const resultVM = str.match( VMVideIdRegexp );
 	const positionYT = resultYT == null ? -1 : str.search( YTVideoIdRegexp );
 	const positionVM = resultVM == null ? -1 : str.search( VMVideIdRegexp );
 	let result = positionYT == -1 ? '' : resultYT[0];
-	result = (positionVM == -1 || positionYT < positionVM ) ? result : resultVM[0];	
+	result = (positionVM != -1 && (positionYT == -1 || positionYT > positionVM )) ? resultVM[0] : result;
 	return result;
 };
