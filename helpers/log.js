56e700db383d84d83a192c6cf89ba423da410dc2
Davlik <dalisoft@mail.ru>
2018-05-01 18:53:21 +0500

    minor patch

--- helpers/log.js
+++ helpers/log.js
@@ -18,66 +18,61 @@ const start = now.getTime();
 const timing = process.env.NODE_ENV === 'dev'; // set to false if you don't want see timing in log
 
 const logToConsole = (data) => {
-	data = commonHelpers.pickEmpty(data);
 	let time;
 
 	if (timing) {
-		time = `Completed in ${((now.getTime() - start) / 1000).toFixed(2)} seconds`;
+		time = `${((now.getTime() - start) / 1000).toFixed(2)} seconds`;
 	}
 	switch (data.log_level) {
-		case 'DEBUG' :
-			console.log(CONSOLE_FONT_COLOR_DEBUG, 'DEBUG: ');
+		case 'DEBUG':
+			if (time) {
+				console.log(CONSOLE_FONT_COLOR_YELLOW, time);
+			}
 			console.log(CONSOLE_FONT_COLOR_DEBUG, data);
+			break;
+		case 'INFO':
 			if (time) {
 				console.log(CONSOLE_FONT_COLOR_YELLOW, time);
 			}
-			break;
-		case 'INFO' :
-			console.log(CONSOLE_FONT_COLOR_YELLOW, 'INFO: ');
 			console.log(CONSOLE_FONT_COLOR_YELLOW, data);
+			break;
+		case 'WARN':
 			if (time) {
 				console.log(CONSOLE_FONT_COLOR_YELLOW, time);
 			}
-			break;
-		case 'WARN' :
-			console.log(CONSOLE_FONT_COLOR_YELLOW, 'WARN: ');
 			console.log(CONSOLE_FONT_COLOR_YELLOW, data);
+			break;
+		case 'ERROR':
+		case 'FATAL':
 			if (time) {
 				console.log(CONSOLE_FONT_COLOR_YELLOW, time);
 			}
-			break;
-		case 'ERROR' :
-		case 'FATAL' :
-			console.log(CONSOLE_FONT_COLOR_RED, data.log_level + ': ');
 			console.log(CONSOLE_FONT_COLOR_RED, data);
-			if (time) {
-				console.log(CONSOLE_FONT_COLOR_YELLOW, time);
-			}
 			break;
-		default :
-			console.log(CONSOLE_RESET, 'LOG: ');
-			console.log(CONSOLE_RESET, data);
+		default:
 			if (time) {
 				console.log(CONSOLE_FONT_COLOR_YELLOW, time);
 			}
+			console.log(CONSOLE_RESET, data);
 	}
 }
 
 const sendToLogger = (data) => {
+	data = commonHelpers.pickEmpty(data);
 	data.server_id = serverName;
 	if (process.env.NODE_ENV != 'production') {
 		data = util.inspect(data, utilInspectOptions); // deeply extract large complicated objects
 		return logToConsole(data);
 	}
 	return axios({
-		method: 'post',
-		headers: {
-			'Content-Type': 'application/json',
-			'x-request-id': generateReqId()
-		},
-		url: 'https://logs.kube-test.isina.com/logs',
-		data
-	})
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json',
+				'x-request-id': generateReqId()
+			},
+			url: 'https://logs.kube-test.isina.com/logs',
+			data
+		})
 		.catch((er) => {
 			console.error(CONSOLE_RESET, 'LOG server not responding, status:', er.response.status);
 			logToConsole(data);
@@ -85,35 +80,35 @@ const sendToLogger = (data) => {
 };
 
 const debug = (message, data) => {
-	if (typeof(data) === 'undefined') data = {};
+	if (typeof (data) === 'undefined') data = {};
 	data.message = message;
 	data.log_level = 'DEBUG';
 	return sendToLogger(data);
 };
 
 const info = (message, data) => {
-	if (typeof(data) === 'undefined') data = {};
+	if (typeof (data) === 'undefined') data = {};
 	data.message = message;
 	data.log_level = 'INFO';
 	return sendToLogger(data);
 };
 
 const warn = (message, data) => {
-	if (typeof(data) === 'undefined') data = {};
+	if (typeof (data) === 'undefined') data = {};
 	data.message = message;
 	data.log_level = 'WARN';
 	return sendToLogger(data);
 };
 
 const error = (message, data) => {
-	if (typeof(data) === 'undefined') data = {};
+	if (typeof (data) === 'undefined') data = {};
 	data.message = message;
 	data.log_level = 'ERROR';
 	return sendToLogger(data);
 };
 
 const fatal = (message, data) => {
-	if (typeof(data) === 'undefined') data = {};
+	if (typeof (data) === 'undefined') data = {};
 	data.message = message;
 	data.log_level = 'FATAL';
 	return sendToLogger(data);
