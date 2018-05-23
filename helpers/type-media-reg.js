257b7644261ea524f71c95494a9ee39579a60369
Tais1990 <tais_kat@list.ru>
2018-05-23 15:58:18 +0500

    bug fix

--- helpers/type-media-reg.js
+++ helpers/type-media-reg.js
@@ -1,18 +1,22 @@
 module.exports = [
 	{
 		reg: /(http[s]?:\/\/){0,1}(?:www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]+)/,
-		type: 'video/youtube'
+		type: 'video',
+		provider: 'youtube'
 	},
 	{
 		reg: /(http[s]?:\/\/){0,1}(?:www\.|player\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/(?:[^/]*)\/videos\/|album\/(?:\d+)\/video\/|video\/|)(\d+)(?:[a-zA-Z0-9_-]+)?/i,
-		type: 'video/vimeo'
+		type: 'video',
+		provider: 'vimeo'
 	},
 	{
 		reg: /(http[s]?:\/\/){0,1}(?:www\.)?([a-zA-Z0-9_-]+)(.mp4)/,
-		type: 'video/mp4'
+		type: 'video',
+		provider: 'mp4'
 	},
 	{
 		reg: /(http[s]?:\/\/){0,1}(?:www\.)?([a-zA-Z0-9_-]+)(.mp3)/,
-		type: 'audio/mp3'
+		type: 'audio',
+		provider: 'mp3'
 	}
 ];
--- helpers/type-media.js
+++ helpers/type-media.js
@@ -1,19 +1,19 @@
 const typeMediaReg = require('./type-media-reg');
+const supportedTypes = ['youtube', 'vimeo'];
 
 module.exports = (str) => {
-	typeMediaReg.map(type => {
-		if (type.type === 'video/youtube') {
-			const result = url.match(type.reg);
-			if (result != null) {
-				return {type: type.type, id: result[result.length - 1]};
-			}
-		}
-		if (type.type === 'video/vimeo') {
-			const result = url.match(type.reg);
-			if (result != null) {
-				return {type: type.type, id: result[result.length - 1]};
-			}
-		}
-		return false;
+	const typeMedia = typeMediaReg.filter(type => (supportedTypes.find(type.provider)));
+	console.log(typeMedia);
+	let result1 = null;
+	typeMedia.map(type => {
+		const result = str.match(type.reg);
+		if (result != null) {
+			result1 = {type: type.type, id: result[result.length - 1]};
+		}		
 	});
+	if (result1 != null) {
+		return result1;
+	} else {
+		return false;
+	}
 };
