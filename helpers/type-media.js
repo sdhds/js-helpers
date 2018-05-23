c451d87521c44ccb16044639cc5aabe5baad2b5c
Tais1990 <tais_kat@list.ru>
2018-05-24 00:52:23 +0500

    bug fix

--- helpers/type-media.js
+++ helpers/type-media.js
@@ -1,28 +1,17 @@
 const typeMediaReg = require('./type-media-reg');
 const supportedTypes = ['youtube', 'vimeo'];
 
-const cont  = (array, element) => {
-	var index = 0;
-	while (index < array.length) {
-		if (element === array[index]) {
-			return true;
-		}
-	}
-	return false;
-}
-
 module.exports = (str) => {
 	const typeMedia = typeMediaReg.filter(type => ( supportedTypes.indexOf(type.provider) > -1));
-	console.log(typeMedia);
-	let result1 = null;
+	let resultObj = null;
 	typeMedia.map(type => {
 		const result = str.match(type.reg);
 		if (result != null) {
-			result1 = {type: type.type, id: result[result.length - 1]};
+			resultObj = {type: type.provider, id: result[result.length - 1]};
 		}		
 	});
-	if (result1 != null) {
-		return result1;
+	if (resultObj != null) {
+		return resultObj;
 	} else {
 		return false;
 	}
