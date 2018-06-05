eb64d0bdf625e4967f13ad4260ac47cea313907e
Tuarisa <tuarisa@gmail.com>
2018-06-05 21:31:07 +0500

    remove paid tracks from calc

--- helpers/profile-score.js
+++ helpers/profile-score.js
@@ -1,10 +1,10 @@
 module.exports = (user_fields) => {
 	const {first_name, last_name, phone,
 		email, is_email_confirmed, about, avatar, cover_photo,
-		has_live, location_now, has_paid_tracks} = user_fields;
+		has_live, location_now} = user_fields;
 	let result = 0;
 	if (first_name && last_name) {
-		result += 10;
+		result += 15;
 	}
 	if (phone) {
 		result += 10;
