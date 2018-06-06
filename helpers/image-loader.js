3e7655e5f10ca12589b8caebafd65f4cef28f454
Renat Berezovsky <renat0014@gmail.com>
2018-06-06 07:00:35 +0400

    Edited image helpers

--- helpers/image-loader.js
+++ helpers/image-loader.js
@@ -3,6 +3,10 @@ const log = require('./log');
 const generateReqId = require('./generate-req-id');
 
 const getImageFromBucket = (imageId, IMAGE_LOADER_API) => {
+    if (!imageId) {
+        return false
+    }
+
     const options = {
         method: 'get',
         headers: {
@@ -17,6 +21,10 @@ const getImageFromBucket = (imageId, IMAGE_LOADER_API) => {
 };
 
 const getImagesFromBucket = (imagesIds, IMAGE_LOADER_API) => {
+    if (!imagesIds || !imagesIds.length) {
+        return false
+    }
+
     const options = {
         method: 'POST',
         headers: {
@@ -37,34 +45,34 @@ const getUrlFromImageObj = image =>
 const getImageIdFromUrl = imageUrl =>
     imageUrl.match(/[0-9a-f]{8}-[0-9a-f]{4}-5[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/g).pop();
 
-const matchUserWithImageUrls = async (user, IMAGE_LOADER_API) => {
-    try {
-        if (user.avatar) {
-            let image = await getImageFromBucket(user.avatar, IMAGE_LOADER_API);
-            image = image.data.data;
-            user.avatar = getUrlFromImageObj(image);
-        }
-
-        if (user.cover_photo) {
-            let image = await getImageFromBucket(user.cover_photo, IMAGE_LOADER_API);
-            image = image.data.data;
-            user.cover_photo = getUrlFromImageObj(image);
-        }
-
-        if (user.photo_ids) {
-            let matchedPhotos = [];
-            for (let photoId of user.photo_ids) {
-                let image = await getImageFromBucket(photoId, IMAGE_LOADER_API);
-                image = image.data.data;
-                matchedPhotos.push(getUrlFromImageObj(image));
-            }
-
-            user.photo_ids = matchedPhotos;
-        }
-    } catch (e) {
-        log.error('matchUserWithImageUrls error');
-    }
-};
+// const matchUserWithImageUrls = async (user, IMAGE_LOADER_API) => {
+//     try {
+//         if (user.avatar) {
+//             let image = await getImageFromBucket(user.avatar, IMAGE_LOADER_API);
+//             image = image.data.data;
+//             user.avatar = getUrlFromImageObj(image);
+//         }
+//
+//         if (user.cover_photo) {
+//             let image = await getImageFromBucket(user.cover_photo, IMAGE_LOADER_API);
+//             image = image.data.data;
+//             user.cover_photo = getUrlFromImageObj(image);
+//         }
+//
+//         if (user.photo_ids) {
+//             let matchedPhotos = [];
+//             for (let photoId of user.photo_ids) {
+//                 let image = await getImageFromBucket(photoId, IMAGE_LOADER_API);
+//                 image = image.data.data;
+//                 matchedPhotos.push(getUrlFromImageObj(image));
+//             }
+//
+//             user.photo_ids = matchedPhotos;
+//         }
+//     } catch (e) {
+//         log.error('matchUserWithImageUrls error');
+//     }
+// };
 
 const removeImages = (images, IMAGE_LOADER_API) => {
     const options = {
@@ -80,24 +88,23 @@ const removeImages = (images, IMAGE_LOADER_API) => {
 
     return axios(options);
 };
-
-const getImagesUrls = (imagesIds, IMAGE_LOADER_API) =>
-    getImagesFromBucket(imagesIds, IMAGE_LOADER_API)
-        .then(response => {
-            if (response && response.data && response.data.data) {
-                return response.data.data.map(image =>
-                    getUrlFromImageObj(image)
-                );
-            }
-
-            return [];
-        });
+//
+// const getImagesUrls = (imagesIds, IMAGE_LOADER_API) =>
+//     getImagesFromBucket(imagesIds, IMAGE_LOADER_API)
+//         .then(response => {
+//             if (response && response.data && response.data.data) {
+//                 return response.data.data.map(image =>
+//                     getUrlFromImageObj(image)
+//                 );
+//             }
+//
+//             return [];
+//         });
 
 module.exports = {
     getImageIdFromUrl,
     getUrlFromImageObj,
-    matchUserWithImageUrls,
     removeImages,
     getImagesFromBucket,
-    getImagesUrls
+    getImageFromBucket
 };
